b7a8f67d22654044393077d7e30236c300f6ca4e
Hiago Souza <ohiagossouza@gmail.com>
2024-04-11 00:29:04 -0300

    add server with express, change express from devDependencies to dependencies

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("<h1>My To-Do</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server active!");
+});
