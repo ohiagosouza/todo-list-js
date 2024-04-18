a4b7cbfdf2dbde47bf05426b5429a0f33fda729e
Hiago Souza <ohiagossouza@gmail.com>
2024-04-18 16:29:52 -0300

    change models folder

--- models/checklist-model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const checklistSchema = mongoose.Schema({
-  checklistName: { type: String, required: true },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
-});
-
-module.exports = mongoose.model("Checklist", checklistSchema);
