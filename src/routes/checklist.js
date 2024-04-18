4413696a990c45e347b1cd9946e5ae65a25d651a
Hiago Souza <ohiagossouza@gmail.com>
2024-04-18 16:27:20 -0300

    update route GET, POST, PUT, DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,21 +1,58 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log(req.body);
-  console.log(`Status code: ${res.statusCode}`);
-  res.status(200).send(req.body);
+const Checklist = require("../models/checklist-model");
+
+// Buscando todos os dados
+router.get("/", async (req, res) => {
+  try {
+    let checklist = await Checklist.find({});
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  console.log(`Status code: ${res.statusCode}`);
-  res.status(200).send(req.body);
+// Buscando dados por ID
+router.get("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get("/:id", (req, res) => {
-  res.send(`ID: ${req.params.id}`);
-  console.log(req.params.id);
+// Salvando dados no servidor
+router.post("/", async (req, res) => {
+  let { checklistName } = req.body;
+
+  try {
+    let checklist = await Checklist.create({ checklistName });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let { checklistName } = req.body;
+
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(req.params.id, { checklistName }, { new: true });
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findByIdAndDelete(req.params.id);
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
 });
 
 module.exports = router;
